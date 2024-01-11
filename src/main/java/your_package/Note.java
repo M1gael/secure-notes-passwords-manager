d2a76adf6636fea862af8d1c1d0c658642697cf6
M1gael <you@example.com>
2024-01-12 00:07:06 +1400

    save progress on generate id , and saving method

--- src/main/java/your_package/Note.java
+++ src/main/java/your_package/Note.java
@@ -1,10 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Note{
     private String title ;
     private String content;
+    private int currentNoteId = generateNoteId();
 
-    public Note(String title , String content){
+    
+
+    //constructor
+    public Note(String title , String content , int currentnoteId){
         this.title = title ; 
         this.content = content;
+        this.currentNoteId ;
+        // Print/save title , content and the return of generate note id
+    }
+
+
+    public int generateNoteId(int lastNoteId){
+        //find a means to extract the note ID from the storage 
+        lastNoteId = 1;
+        return lastNoteId++;
     }
     
     @Override
