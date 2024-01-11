873c8448ba19043c9ca28333e49f89baa6a7ead2
M1gael <you@example.com>
2024-01-12 03:45:37 +1400

    Succesfully Saved to a .txt

--- src/main/java/your_package/Note.java
+++ src/main/java/your_package/Note.java
@@ -5,25 +5,47 @@ import java.io.IOException;
 public class Note{
     private String title ;
     private String content;
-    private int currentNoteId = generateNoteId();
+    private int currentNoteId;
 
     
 
     //constructor
-    public Note(String title , String content , int currentnoteId){
+    public Note(String title , String content ){
         this.title = title ; 
         this.content = content;
-        this.currentNoteId ;
+        this.currentNoteId = generateNoteId();
+        saveNote();
         // Print/save title , content and the return of generate note id
     }
 
 
-    public int generateNoteId(int lastNoteId){
-        //find a means to extract the note ID from the storage 
-        lastNoteId = 1;
+    private int generateNoteId(){
+        //find a means to extract the last note ID from the storage 
+        int lastNoteId = 1;
         return lastNoteId++;
     }
+
+
+    private void saveNote(){
     
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("resources/data/SavedNotes.txt", true))) {
+            writer.write(String.format("%d %s %s%n" , currentNoteId , title , content));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void editNote(){
+
+    }
+    
+    public static void loadNotes(){
+
+    }
+
     @Override
     public String toString(){
         return "Title: " + title + "\n" + "Content: " + content;
