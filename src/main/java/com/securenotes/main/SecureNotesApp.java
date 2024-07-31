66e51e13965f2207d62190d74530742a9ffedd66
M1gael <smit.migael.johannes@gmail.com>
2024-07-31 03:19:58 +0200

    Quick package comply test and sync

--- src/main/java/com/securenotes/main/SecureNotesApp.java
+++ src/main/java/com/securenotes/main/SecureNotesApp.java
@@ -1,4 +1,9 @@
 package com.securenotes.main;
 
+import com.securenotes.ui.NoteEditScreen;
+
 public class SecureNotesApp {
+    public static void main(String[] args) {
+        NoteEditScreen newEdit = new NoteEditScreen();
+    }
 }
--- src/main/java/com/securenotes/ui/NoteEditScreen.java
+++ src/main/java/com/securenotes/ui/NoteEditScreen.java
@@ -1,4 +1,14 @@
 package com.securenotes.ui;
 
-public class NoteEditScreen {
+import javax.swing.*;
+
+public class NoteEditScreen extends JFrame {
+
+    public NoteEditScreen(){
+        this.setTitle("Editing....");
+        this.setSize(500 , 500);
+        this.setVisible(true);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
